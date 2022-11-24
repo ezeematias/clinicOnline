13eff302771623aab2c49332c6099cb8441ac1a6
Ezequiel Unía <eunia69@gmail.com>
2022-11-23 21:51:40 -0300

    change

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
 import { AuthService } from 'src/app/services/auth.service';
 import { UsersService } from 'src/app/services/users.service';
 
@@ -17,50 +16,7 @@ export class ReportsComponent implements OnInit {
   days: string[] = [];
 
   ngOnInit(): void {
-
-    this.chart = new Chart('canvas', {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'red',
-            'red',
-            'red',
-            'red',
-            'red',
-            'red',
-            'red',
-          ],
-        },
-        {
-          data: [2, 9, 1, 1, 2, 2],
-          backgroundColor: [
-            '#00ffff',
-            '#00ffff',
-            '#00ffff',
-            '#00ffff',
-            '#00ffff',
-            '#00ffff',
-            '#00ffff',
-          ],
-        }
-        ]
-      }, options: {
-        scales: {
-          x: { display: true },
-          y: {
-            display: true,
-            beginAtZero: true
-          }
-        }
-      }
-    })
+    
   }
-
-
-
 
 }
